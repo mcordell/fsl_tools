e5854d6d98e074600d6022c214d1b1803ca57afa
Michael Cordell <surpher@gmail.com>
2012-05-25 09:14:48 -0700

    Added new parsing methods to fsf_file, helper classes for fsf_file Moved ethods for excel_results

--- excel_results.py
+++ excel_results.py
@@ -3,6 +3,37 @@ import xlwt, os, xlrd, re
 from xlutils import copy
 from PIL import Image
 
+def write_row(row_name,vertical_start,horizontal_start,me_root_path,worksheet,horizontal_move,scale_factor):
+    """ Writes a row in a given worksheet, searching though a me_root_path, using
+        the results images within that root directory.row_name is written above each image in the row
+        Row writing starts at horiztonal/vertical cell position within the worksheet. Horizontal position
+        moves with the horiztonal move
+    """
+    files_list=os.listdir(me_root_path)
+    for cope_name in files_list:
+        copenumber_match=re.search("cope\d+",cope_name)
+        if copenumber_match:
+            cope_dir=os.path.join(me_root_path,cope_name)
+            if os.path.isdir(cope_dir):
+                worksheet.write(vertical_start-1,horizontal_start,row_name)
+                rendered_thresh=os.path.join(cope_dir,"rendered_thresh_zstat1.png")
+                cluster_max_path=os.path.join(cope_dir,"cluster_zstat1_std.txt")
+                if os.path.isfile(rendered_thresh) and os.path.isfile(cluster_max_path):
+                    with file(cluster_max_path,'r') as cluster_file:
+                        cluster_lines= cluster_file.readlines()
+                    if len(cluster_lines) > 1:
+                        img = Image.open(rendered_thresh)
+                        (width,height)=img.size
+                        new_width=int(width*scale_factor)
+                        new_height=int(height*scale_factor)
+                        resized=img.resize((new_width,new_height))
+                        resized.save("resized_temp.bmp")
+
+                        worksheet.insert_bitmap("resized_temp.bmp",vertical_start,horizontal_start)
+                        os.remove("resized_temp.bmp")
+            horizontal_start+=horizontal_move
+
+
 class excel_results:
     def __init__(self, higher_level_names, lower_level_names, ME_paths, template_path, excel_outpath):
         #TODO everything is done at intialization, need to put this in an another method
@@ -26,36 +57,6 @@ class excel_results:
             return cope_number
 
 
-    def write_row(self,row_name,vertical_start,horizontal_start,me_root_path,worksheet):
-        """ Writes a row in a given worksheet, searching though a me_root_path, using
-            the results images within that root directory.row_name is written above each image in the row
-            Row writing starts at horiztonal/vertical cell position within the worksheet. Horizontal position
-            moves with the self variable self.horiztonal move
-        """
-        files_list=os.listdir(me_root_path)
-        for cope_name in files_list:
-            copenumber_match=re.search("cope\d+",cope_name)
-            if copenumber_match:
-                cope_dir=os.path.join(me_root_path,cope_name)
-                if os.path.isdir(cope_dir):
-                    worksheet.write(vertical_start-1,horizontal_start,row_name)
-                    rendered_thresh=os.path.join(cope_dir,"rendered_thresh_zstat1.png")
-                    cluster_max_path=os.path.join(cope_dir,"cluster_zstat1_std.txt")
-                    if os.path.isfile(rendered_thresh) and os.path.isfile(cluster_max_path):
-                        with file(cluster_max_path,'r') as cluster_file:
-                            cluster_lines= cluster_file.readlines()
-                        if len(cluster_lines) > 1:
-                            img = Image.open(rendered_thresh)
-                            (width,height)=img.size
-                            new_width=int(width*self.scale_factor)
-                            new_height=int(height*self.scale_factor)
-                            resized=img.resize((new_width,new_height))
-                            resized.save("resized_temp.bmp")
-
-                            worksheet.insert_bitmap("resized_temp.bmp",vertical_start,horizontal_start)
-                            os.remove("resized_temp.bmp")
-                horizontal_start+=self.horizontal_move
-
 
     def main(self):
         #open template file
@@ -78,7 +79,7 @@ class excel_results:
                 except:
                     "Higher level Mixed effects not found in lower level copes. Mismatch?"
                 me=organize_MEs[i]
-                self.write_row(name,vert_start,horz_start,me,ws)
+                write_row(name,vert_start,horz_start,me,ws,self.horizontal_move,self.scale_factor)
                 vert_start+=self.vertical_move
         wb.save(self.excel_outpath)
 
--- fsf_file.py
+++ fsf_file.py
@@ -11,14 +11,19 @@ class fsf_file:
         self.ME_TYPE=3
         self.FIRST_TYPE=1
         self.PRE_TYPE=0
-
         self.FilePath=path
         self.fsf_lines=self.load_file()
-        self.values_dict=parse_to_dict(self.fsf_lines)
-        self.type=self.get_type()
-
         if self.fsf_lines:
+            self.values_dict=parse_to_dict(self.fsf_lines)
             self.type=self.get_type()
+            if type:
+                #populate the values common to all fsf's
+                self.output_path=self.get_value("outputdir")
+                self.analysis_name=self.get_analysis_name(self.output_path)
+                self.z_value=self.get_value("z_thresh")
+                self.p_value=self.get_value("prob_thresh")
+                #self.fill_values_from_dict()
+
             self.parsed=self.parse_design_file(self.fsf_lines,self.type)
             self.out_lines=self.parsed_to_csv_lines(self.parsed,self.type)
             self.explode_parsed(self.parsed)
@@ -38,6 +43,7 @@ class fsf_file:
         #determine fsf file type
         high_value=self.get_value("level")
         analysis_type=self.get_value("analysis")
+        type=None
         if (high_value != "1"):
             higher_level_type=self.get_value("mixed_yn")
             if higher_level_type == "0" or higher_level_type == "2" or higher_level_type == "1":
@@ -58,9 +64,156 @@ class fsf_file:
         except:
             print "Could not open fsf"
 
+    def get_analysis_name(self,output_dir_string):
+        analysis_name=None
+        run=None
+        if self.type == self.ME_TYPE:
+            run=re.search("ME",output_dir_string)
+        elif self.type == self.FIRST_TYPE or type == self.PRE_TYPE:
+            run=re.search("r\d\/",output_dir_string)
+        elif self.type == self.FE_TYPE:
+            run=re.search("FE\d*",output_dir_string)
+        if run:
+            analysis_name=self.get_fsf_value(output_dir_string,run.end())
+            if type == self.ME_TYPE:
+                analysis_name=self.strip_cope(analysis_name)
+        return analysis_name
 
+    def binary_value_to_yes_no(self,value):
+        if value == "1":
+            value = "Y"
+        elif value is not None:
+            value = "N"
+        return value
 
+    def fill_pre(self):
+        self.tr=self.get_value("tr")
+        self.smoothing=self.get_value("smooth")
+        self.number_volumes=self.get_value("npts")
+        self.removed_volumes=self.get_value("ndelete")
+        self.motion_correction=self.binary_value_to_yes_no(self.get_value("mc"))
+        self.brain_thresh=self.get_value("brain_thresh")
+        input_value=self.get_value("feat_files(1)")
+        if input_value:
+            self.input_file=self.strip_root()
 
+    def fill_first_level(self):
+        number_of_evs=int(self.get_value("evs_orig"))
+        number_of_copes=int(self.get_value("ncon_orig"))
+        design_matrix=[['0' for col in range(number_of_evs+2)] for row in range(number_of_copes+1)]
+        design_matrix[0][0]="Cope #"
+        design_matrix[0][1]="Cope Name"
+        self.evs=dict()
+        self.cons=dict()
+        for ind in range(1,number_of_evs+1):
+            index=str(ind)
+            ev_1=ev()
+            ev_1.name=self.get_value("evtitle"+index)
+            ev_1.file_path=self.get_value("custom"+index)
+            ev_1.set_convolution(self.get_value("convolve"+index))
+            ev_1.temporal_deriv=self.binary_value_to_yes_no(self.get_value("deriv_yn"+index))
+            ev_1.temporal_filtering=self.binary_value_to_yes_no(self.get_value("tempflit_yn"+index))
+            design_matrix[0][ind+1]=ev_1.name
+            self.evs[index]=ev_1
+
+        for ind in range(1,number_of_copes+1):
+            index=str(ind)
+            con=contrast()
+            con.name=self.get_value("conname_orig."+index)
+            con.ev_list=dict()
+            real_cope_index=1
+            design_matrix[ind][0]=index
+            design_matrix[ind][1]=con.name
+            for j in range (1,number_of_evs+1):
+                ev_value=self.get_value("con_real"+index+"."+str(real_cope_index))
+                con.ev_list[str(j)]=ev_value
+                design_matrix[ind][j+1]=ev_value
+                if self.evs[str(j)].temporal_deriv == "Y":
+                    real_cope_index +=2
+                else:
+                    real_cope_index+=1
+            self.cons[index]=con
+        self.design_matrix=design_matrix
+
+    def fill_FE(self):
+        number_of_evs=int(self.get_value("evs_orig"))
+        number_of_copes=int(self.get_value("ncon_orig"))
+        self.number_inputs=self.get_value("npts")
+        self.inputs=dict()
+        self.stripped_inputs=dict()
+        #prep matrices
+        regress_matrix=[['0' for col in range(self.number_inputs+1)] for row in range(number_of_evs+1)]
+        design_matrix=[['0' for col in range(number_of_evs+2)] for row in range(number_of_copes+1)]
+        design_matrix[0][0]="Cope #"
+        design_matrix[0][1]="Cope Name"
+        regress_matrix[0][0]="EV Name"
+        #fill indices
+        for ind in range(1,self.number_inputs+1):
+            index=str(ind)
+            input_value=self.get_value("feat_files("+index+")")
+            self.inputs[index]=input_value
+            input_value=input_value.strip("\n")
+            input_value=input_value.strip('\"')
+            self.stripped_inputs[index]=input_value
+            regress_matrix[0][ind]=index
+        #populate evs and regressor matrix
+        self.evs=dict()
+        for ind in range(1,number_of_evs+1):
+            index=str(ind)
+            ev_temp=ev()
+            ev_temp.name=self.get_value("evtitle"+index)
+            regress_matrix[ind][0]=ev_temp.name
+            design_matrix[0][ind+1]=ev_temp.name
+            ev_temp.input_list=dict()
+            for j in range(1,self.number_inputs+1):
+                jstr=str(j)
+                input_val=self.get_value("evg"+jstr+"."+index)
+                regress_matrix[ind][j]=input_val
+                ev_temp.input_list[jstr]=input_val
+            seld.evs[index]=ev_temp
+
+        #populate the copes and contrast matrix
+        self.cons=dict()
+        for ind in range(1,number_of_copes+1):
+            index=str(ind)
+            con=contrast()
+            con.name=self.get_value("conname_orig."+index)
+            con.ev_list=dict()
+            real_cope_index=1
+            design_matrix[ind][0]=index
+            design_matrix[ind][1]=con.name
+            for j in range (1,number_of_evs+1):
+                ev_value=self.get_value("con_real"+index+"."+str(real_cope_index))
+                con.ev_list[str(j)]=ev_value
+                design_matrix[ind][j+1]=ev_value
+                real_cope_index+=1
+            self.cons[index]=con
+        self.design_matrix=design_matrix
+        self.regressor_matrix=regress_matrix
+
+    def fill_ME(self):
+        self.number_subjects=self.get_value("npts")
+        self.inputs=dict()
+        self.stripped_inputs=dict()
+        for ind in range(1,self.number_inputs+1):
+            index=str(ind)
+            input_value=self.get_value("feat_files("+index+")")
+            self.inputs[index]=input_value
+            input_value=input_value.strip("\n")
+            input_value=input_value.strip('\"')
+            input_value=self.strip_cope(input_value)
+            self.stripped_inputs[index]=input_value
+            regress_matrix[0][ind]=index
+        
+    def fill_values_from_dict(self):
+        if self.type == self.PRE_TYPE:
+            self.fill_pre()
+        elif self.type == self.FIRST_TYPE:
+            self.fill_first_level()
+        elif self.type == self.FE_TYPE:
+            self.fill_FE()
+        elif self.type == self.ME_TYPE:
+            self.fill_ME()
 
 
 
@@ -406,7 +559,6 @@ class fsf_file:
 
     def strip_root(self,line):
         run=re.search("r\d\/",line)
-    
         if run:
             out=self.get_fsf_value(line,run.end())
             out='./'+out
@@ -536,3 +688,44 @@ class fsf_file:
             self.analysis_name,self.output_path,self.feat_paths,self.count,self.design_matrix,self.regressor_matrix,self.ev_names,self.cope_names,self.first_example_dir=parsed_data
         elif self.type == self.PRE_TYPE:
             self.analysis_name,self.output_path,self.tr,self.total_volumes,self.deleted,self.in_file,self.motion_correction,self.brain_thresh,self.smoothing=parsed_data
+
+    def get_fsf_value(self,fsf_line, end):
+        """ return a value from an fsf line
+            fsf lines have a perdictable structure, where vaiable is defined and then value follows
+            this function returns of a clean copy of the variable in supplied fsf_line, where value is everything in the
+            line after the supplied variable end. newline characters, " , and leading spaces are stripped
+        """
+        value=fsf_line[end:len(fsf_line)]
+        value=value.strip("\n")
+        value=value.strip()
+        value=value.replace('"','')
+        return value
+
+    def get_fsf_indice(self,fsf_variable_def):
+        """ return a the variable indice from an fsf line variable declaration
+            fsf variables are defined as set fmri(***variable type***#indice)
+            this function returns the indice of the supplied variable definition
+        """
+        match=re.search('\d+', fsf_variable_def)
+        return match.group()
+
+class ev:
+    Name = "EV"
+    def set_convolution(self,convolution_value):
+        conv_text={
+            "0" : "none",
+            "1" : "Gaussian",
+            "2" : "Gamma",
+            "3" : "Double-Gamma HRF",
+            "4" : "Gamma basis functions",
+            "5" : "Sine basis functions",
+            "6" : "FIR basis functions",
+            }
+        if convolution_value is None:
+            self.convolution=None
+        else:
+            self.convolution=conv_text[convolution_value]
+class contrast:
+    Name = "Contrast"
+
+
