9bf496517cc776c48e92951ce903bd4bee2b96cb
Michael Cordell <surpher@gmail.com>
2012-07-16 16:02:39 -0700

    created mask to image script

---
+++ mask_to_image.py
@@ -0,0 +1,10 @@
+__author__ = 'michael'
+
+#Parse options
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--featpath')
+parser.add_argument('-o', '--out')
+parser.add_argument('-a', '--analysis')
+parser.add_argument('-c', '--config')
+parser.add_argument('-m','--manual_search')
+parser.add_argument('-s','--simple_output')
