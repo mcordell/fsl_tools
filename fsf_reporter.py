56a764d6bf30dea89d831c6c7d2a32ce20dcec3f
Michael Cordell <surpher@gmail.com>
2012-11-14 17:07:40 -0800

    added die method, catches for no output path and bad analysis path

--- fsf_reporter.py
+++ fsf_reporter.py
@@ -3,7 +3,14 @@ import argparse, os, re, ConfigParser
 from utils import fsf_to_csv,fsf_to_one_column,write_report,combine_for_csv, get_input_fsf,combine_left_right
 from fsf_file import fsf_file
 from excel_results import excel_results
-from ConfigParser import NoSectionError
+from ConfigParser import NoSectionError, NoOptionError
+def die(message):
+    if message:
+        print message
+    else:
+        print "Unexplained Exit"
+    exit()
+
 
 def main():
     """
@@ -11,6 +18,7 @@ def main():
     """
     global first_level_dir, ME_folders, first_fsf, FE_fsf, one_col, ME_csv, FE_csv, preproc_csv, first_csv, FE_dir, FE_dir, ME_dir, ME_dir, out_lines
     template_path="template2.xls"
+    height_of_all_lines=0
 
     #Parse options
     parser = argparse.ArgumentParser()
@@ -25,12 +33,17 @@ def main():
     feat_folder_path=args.featpath
     simple_output=args.simple_output
     analysis=args.analysis
+
     if args.manual_search:
         search_down_method=0
     else:
         search_down_method=1
     if args.out is None:
-        out_path=analysis
+        if analysis:
+            out_path=analysis
+        else:
+            out_path=''
+            die("Need to set output prefix with -o arg when using path to fsf (-p) arg")
     else:
         out_path=args.out
 
@@ -38,19 +51,15 @@ def main():
     if config_file_path is None:
         config_file_path="example.cfg"
     if feat_folder_path and analysis:
-        print "Please use either -p <path to single feat folder> or"
-        print "or -a <analysis name>. Not both."
-        exit()
-
+        die("Please use either -p <path to single feat folder> or\n"+
+            "or -a <analysis name>. Not both.")
 
     # read configuration file
     try:
         f = open(config_file_path,'r')
         f.close()
     except IOError:
-        print "Config file path not valid. Please specify a valid path"
-        exit()
-
+        die("Config file path not valid. Please specify a valid path")
 
     config = ConfigParser.RawConfigParser()
     config.read(config_file_path)
@@ -60,18 +69,10 @@ def main():
         FE_dir = config.get('Analysis Directories', 'fe_dir')
         ME_dir = config.get('Analysis Directories', 'me_dir')
         template_path=config.get('Analysis Directories', 'template')
-    except NoSectionError:
-        print "Config file appears to be corrupt, regenerate with create_config or specify new path"
-        exit()
-
-
-
-
-
-    height_of_all_lines=0
+    except NoSectionError or NoOptionError:
+        die("Config file appears to be corrupt, regenerate with create_config or specify new path")
 
     #find the location of the feat folders within the directories from the config file
-
     if analysis:
         ###Search down switch
         if search_down_method:
@@ -84,86 +85,90 @@ def main():
                     combined=os.path.join(ME_dir,folder)
                     if os.path.isdir(combined):
                         ME_folders.append(combined)
-            if ME_folders:
-                #load any ME directory
-                ME_fsf=fsf_file(os.path.join(ME_folders[0],'design.fsf'))
-                ME_inputs=ME_fsf.inputs
-                fe_fsf_path=''
-                me_input_count=1
-                while not fe_fsf_path:
-                    try:
-                        fe_fsf_path=ME_inputs[str(me_input_count)].strip('\"')+'/design.fsf'
-                    except KeyError:
-                        break
-                    if not os.path.isfile(fe_fsf_path):
-                        me_input_count+=1
-                        fe_fsf_path=''
-                FE_fsf=fsf_file(fe_fsf_path)
-                FE_inputs=FE_fsf.inputs
-                other_FES=list()
-                input_fsf_path=get_input_fsf(FE_inputs)
+
+            #Catch bad analysis name at ME Level
+            if not ME_folders:
+                die("No analysis folders found at ME level. Do you have the right analysis name?")
+
+            #load any ME directory
+            ME_fsf=fsf_file(os.path.join(ME_folders[0],'design.fsf'))
+            ME_inputs=ME_fsf.inputs
+            fe_fsf_path=''
+            me_input_count=1
+            while not fe_fsf_path:
+                try:
+                    fe_fsf_path=ME_inputs[str(me_input_count)].strip('\"')+'/design.fsf'
+                except KeyError:
+                    break
+                if not os.path.isfile(fe_fsf_path):
+                    me_input_count+=1
+                    fe_fsf_path=''
+            FE_fsf=fsf_file(fe_fsf_path)
+            FE_inputs=FE_fsf.inputs
+            other_FES=list()
+            input_fsf_path=get_input_fsf(FE_inputs)
+            input_fsf=fsf_file(input_fsf_path)
+            while input_fsf.type == input_fsf.FE_TYPE:
+                other_FES.append(input_fsf)
+                input_fsf_path=get_input_fsf(input_fsf.inputs)
                 input_fsf=fsf_file(input_fsf_path)
-                while input_fsf.type == input_fsf.FE_TYPE:
-                    other_FES.append(input_fsf)
-                    input_fsf_path=get_input_fsf(input_fsf.inputs)
-                    input_fsf=fsf_file(input_fsf_path)
-                first_fsf=input_fsf
-                one_col=list()
-                if first_fsf.type == first_fsf.FIRST_TYPE:
-                    one_col.extend(fsf_to_one_column(first_fsf))
+            first_fsf=input_fsf
+            one_col=list()
+            if first_fsf.type == first_fsf.FIRST_TYPE:
+                one_col.extend(fsf_to_one_column(first_fsf))
+                one_col.append(",\n")
+                first_csv=fsf_to_csv(first_fsf)
+                if hasattr(first_fsf,'preproc'):
+                    preprocdir=os.path.join(first_level_dir,first_fsf.preproc)
+                    preproc_fsf=fsf_file((os.path.join(preprocdir,'design.fsf')))
+                    preproc_csv=fsf_to_csv(preproc_fsf)
+                    one_col.extend(fsf_to_one_column(preproc_fsf))
                     one_col.append(",\n")
-                    first_csv=fsf_to_csv(first_fsf)
-                    if hasattr(first_fsf,'preproc'):
-                        preprocdir=os.path.join(first_level_dir,first_fsf.preproc)
-                        preproc_fsf=fsf_file((os.path.join(preprocdir,'design.fsf')))
-                        preproc_csv=fsf_to_csv(preproc_fsf)
-                        one_col.extend(fsf_to_one_column(preproc_fsf))
-                        one_col.append(",\n")
-                    else:
-                        preproc_csv=None
                 else:
-                    first_csv=None
                     preproc_csv=None
-                    print "First level not loaded or design file is corrupt. Not adding to output"
+            else:
+                first_csv=None
+                preproc_csv=None
+                print "First level not loaded or design file is corrupt. Not adding to output"
 
-                if FE_fsf.type == FE_fsf.FE_TYPE:
-                    one_col.append(",\n")
-                    FE_csv=fsf_to_csv(FE_fsf)
-                    one_col.extend(fsf_to_one_column(FE_fsf))
-                    one_col.append(",\n")
+            if FE_fsf.type == FE_fsf.FE_TYPE:
+                one_col.append(",\n")
+                FE_csv=fsf_to_csv(FE_fsf)
+                one_col.extend(fsf_to_one_column(FE_fsf))
+                one_col.append(",\n")
+            else:
+                FE_csv=None
+                print "No fixed effects loaded, data will not be included in output"
+
+            if ME_fsf:
+                ME_csv=fsf_to_csv(ME_fsf)
+                one_col.extend(fsf_to_one_column(ME_fsf))
+                one_col.append(",\n")
+            else:
+                ME_csv=None
+                print "No Mixed effects loaded, data will not be included in output"
+
+            #out_lines=combine_for_csv(first_csv,height_of_all_lines,preproc_csv,FE_csv,ME_csv)
+            out_lines=list()
+            if first_csv:
+                if preproc_csv:
+                    out_lines=combine_left_right(preproc_csv[0],first_csv[0])
                 else:
-                    FE_csv=None
-                    print "No fixed effects loaded, data will not be included in output"
+                    out_lines=first_csv[0]
+            if FE_csv:
+                if other_FES:
+                    size_of_others=len(other_FES)-1
+                    print size_of_others
+                    while size_of_others >= 0:
 
-                if ME_fsf:
-                    ME_csv=fsf_to_csv(ME_fsf)
-                    one_col.extend(fsf_to_one_column(ME_fsf))
-                    one_col.append(",\n")
-                else:
-                    ME_csv=None
-                    print "No Mixed effects loaded, data will not be included in output"
-
-                #out_lines=combine_for_csv(first_csv,height_of_all_lines,preproc_csv,FE_csv,ME_csv)
-                out_lines=list()
-                if first_csv:
-                    if preproc_csv:
-                        out_lines=combine_left_right(preproc_csv[0],first_csv[0])
-                    else:
-                        out_lines=first_csv[0]
-                if FE_csv:
-                    if other_FES:
-                        size_of_others=len(other_FES)-1
                         print size_of_others
-                        while size_of_others >= 0:
+                        temp_csv=fsf_to_csv(other_FES[size_of_others])
+                        out_lines=combine_left_right(out_lines,temp_csv[0])
+                        size_of_others -= 1
 
-                            print size_of_others
-                            temp_csv=fsf_to_csv(other_FES[size_of_others])
-                            out_lines=combine_left_right(out_lines,temp_csv[0])
-                            size_of_others -= 1
-
-                    out_lines=combine_left_right(out_lines,FE_csv[0])
-                if ME_csv:
-                    out_lines=combine_left_right(out_lines,ME_csv[0])
+                out_lines=combine_left_right(out_lines,FE_csv[0])
+            if ME_csv:
+                out_lines=combine_left_right(out_lines,ME_csv[0])
         else:
         ###Old method of searching
             ME_list=os.listdir(os.path.join(ME_dir))
@@ -265,7 +270,7 @@ def main():
             key,contrast=item
             first_cope_names[key]=contrast.name
 
-        if simple_output is not None:
+        if simple_output is None:
             excel=excel_results(fe_cope_names,first_cope_names, ME_folders, template_path,excel_output_path)
             excel.main()
     elif feat_folder_path :
