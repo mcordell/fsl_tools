609a32c29cfa0d35a35b1195ed623c2f6b5d8511
Michael Cordell <surpher@gmail.com>
2012-12-25 00:45:57 -0800

    added excel wrapper

---
+++ link_fixer.py
@@ -0,0 +1,27 @@
+#! /Library/Frameworks/Python.framework/Versions/Current/bin/python
+import re,os
+
+
+
+if __name__ == "__main__":
+    regex_phrase=raw_input('Enter match phrase for images:\n')
+    print regex_phrase
+
+    files=os.listdir(os.curdir)
+    match_list=list()
+    for file in files:
+       match=re.search(regex_phrase,file)
+       if match:
+           match_list.append(file)
+           print file
+
+    sorted_match_list=sorted(match_list)
+
+    out_name=raw_input('Enter output name:\n')
+    output_string="fslmerge -t "+out_name
+    for file_name in sorted_match_list:
+        output_string=output_string+" "+file_name
+
+    print output_string
+
+
