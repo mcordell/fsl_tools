27d00c6949f1eb99f4e22afeabf7e0ac25f5281d
mcordell <surpher@gmail.com>
2012-12-26 01:37:03 -0800

    Added configuration class

---
+++ fsl_tools/configuration.py
@@ -0,0 +1,31 @@
+import ConfigParser
+from ConfigParser import NoSectionError, NoOptionError
+
+class Configuration:
+    """docstring for Configuration"""
+    name = "Configuration"
+
+    def __init__(self, config_path):
+        try:
+            f = open(config_path,'r')
+            f.close()
+        except IOError:
+            print ("Config file path not valid. Please specify a valid path")
+            raise IOError
+        self.config = ConfigParser.RawConfigParser()
+        self.config.read(config_path)
+        self.first_level_dir=self.read_option('Analysis Directories','first_level_dir') 
+        self.FE_dir=self.read_option('Analysis Directories','fe_dir') 
+        self.ME_dir=self.read_option('Analysis Directories','me_dir') 
+        self.template_path=self.read_option('Analysis Directories','template')
+        
+    def read_option(self,category, key):
+        try:
+            value = self.config.get(category,key)
+        except NoSectionError as e: 
+            print ("Config file appears to not have a section "+category)
+            raise e
+        except NoOptionError as e:
+            print ("Config file appears to not have a value "+key+" in the section "+category)
+            raise e
+        return value
