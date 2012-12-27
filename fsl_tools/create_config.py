dbccdc74c36af137bcc183bbabe08a34396b6432
mcordell <surpher@gmail.com>
2012-12-26 17:46:37 -0800

    added match variables

--- fsl_tools/create_config.py
+++ fsl_tools/create_config.py
@@ -3,9 +3,9 @@ import ConfigParser
 
 def main():
     config = ConfigParser.RawConfigParser()
+    
     config.add_section('directory structure')
     config.set('directory structure','experiment_root',"/mnt/Storage/TAF_fanal/")
-
 
     config.add_section('Analysis Directories')
     config.set('Analysis Directories', 'first_level_dir', "/Volumes/storage/TAF_fanal/FS/Ctrl/x305/r2")
@@ -23,9 +23,9 @@ def main():
     config.add_section('fsl_directories')
     config.set("fsl_directories",'standard_brain',"/usr/local/fsl/data/standard/MNI152_T1_2mm_brain")
 
-    config.add_section('run_to_atlas')
-    config.set("run_to_atlas",'me_pattern',"_cope")
-
+    config.add_section('Match Patterns')
+    config.set('Match Patterns','me_pattern', "_cope")
+    config.set('Match Patterns','cope_pattern', "cope\d+")
 
     with open('example.cfg', 'wb') as configfile:
         config.write(configfile)
